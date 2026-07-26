c2980329caea1fe229899845daad18698c5f9a33
Syed Salman Ali Zaidi <syedsalman1823@gmail.com>
2026-07-26 23:22:47 +0500

    S: Deleted files

--- ExpenseTracker.Domain/Repositories/IExpenseRepository.cs
+++
@@ -1,9 +0,0 @@
-using ExpenseTracker.Domain.Entities;
-
-namespace ExpenseTracker.Domain.Repositories;
-
-public interface IExpenseRepository: IRepository<Transaction>
-{
-    Task<IEnumerable<Transaction>> GetByUserIdAsync(Guid userId);
-    Task<IEnumerable<Transaction>> GetByCategoryIdAsync(Guid categoryId);
-}
